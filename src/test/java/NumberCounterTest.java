f8c5a217baa67287405ab563d7f8f6a33499a197
SaraQian <abigail830@163.com>
2019-03-14 16:18:20 +0800

    Step1. adding test case

---
+++ src/test/java/NumberCounterTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class NumberCounterTest {
+
+    @Test
+    void should_get_5_even_count_when_input_1_to_11() {
+        NumberCounter numberCounter = new NumberCounter(1,2,3,4,5,6,7,8,9,10,11);
+        assertEquals(5, numberCounter.count(NumberCounter.EVEN));
+    }
+
+    @Test
+    void should_get_6_odd_count_when_input_1_to_11() {
+        NumberCounter numberCounter = new NumberCounter(1,2,3,4,5,6,7,8,9,10,11);
+        assertEquals(6, numberCounter.count(NumberCounter.ODD));
+    }
+
+    @Test
+    void should_get_positive_count() {
+        NumberCounter numberCounter = new NumberCounter(-1,-2,3,4,5,6,7,8,9,10,11);
+        assertEquals(9, numberCounter.count(NumberCounter.POSITIVE));
+    }
+
+    @Test
+    void should_get_negative_count() {
+        NumberCounter numberCounter = new NumberCounter(-1,-2,3,4,5,6,7,8,9,10,11);
+        assertEquals(2, numberCounter.count(NumberCounter.NEGATIVE));
+    }
+}
